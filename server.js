2fafaf6ee933dde4a91327a08d9c9b898f7637c7
Ernesto Custodio <ernestojosecg@gmail.com>
2020-10-09 12:53:53 +0200

    Server

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import Pusher from 'pusher'
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up mongoose
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
